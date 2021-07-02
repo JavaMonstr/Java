72d05cb35eed45917bd0aa99ed2cd6645c25671e
JavaMonstr <postnoff@list.ru>
2021-07-02 10:48:13 +0600

    Merge "5"
    This reverts commit 50409ebd8e105fc7f87ccf3ed7a87ced83de5c4e.

--- Test.java
+++ Test.java
@@ -2,8 +2,11 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!1"); 
 		System.out.println("Hello, World!2"); 
+<<<<<<< HEAD
 		System.out.println("Hello, World!3"); 
 		System.out.println("Hello, World!4");
 		System.out.println("Hello, World!5"); 		
+=======
+>>>>>>> parent of 4db743a (3)
 	    }
 }
