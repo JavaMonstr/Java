50409ebd8e105fc7f87ccf3ed7a87ced83de5c4e
JavaMonstr <postnoff@list.ru>
2021-07-02 10:02:43 +0600

    5

--- Test.java
+++ Test.java
@@ -3,6 +3,7 @@ class HelloWorld {
         System.out.println("Hello, World!1"); 
 		System.out.println("Hello, World!2"); 
 		System.out.println("Hello, World!3"); 
-		System.out.println("Hello, World!4"); 
+		System.out.println("Hello, World!4");
+		System.out.println("Hello, World!5"); 		
 	    }
 }
